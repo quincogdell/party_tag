798dfe88825887044476ede8937311f953ec7783
Quin Cogdell <quincogdell@gmail.com>
2013-11-11 17:43:07 -0500

    fixed typo in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ e1 = Event.new(title: 'party tag programming week')
 
 h1 = HashTag.new(tag_name: '#partytagworkweed')
 
-scap1 = Scrapbook.new()
+scrap1 = Scrapbook.new()
 
 e1.hash_tags << h1
 e1.scrapbooks << scrap1
