01c6240d2b55022c1ba7ce9770be497c15432a55
Enoch Riese <enoch.riese@gmail.com>
2013-11-12 12:39:12 -0500

    got events edit form working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -44,6 +44,7 @@ class EventsController < ApplicationController
   end
   def edit
     @event = Event.find(params[:id])
+    @tags = @event.hash_tags
   end
   def update
     @event = Event.find(params[:id])
