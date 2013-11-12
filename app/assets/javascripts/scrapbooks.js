5ffed3a4caa91cfbd8951ead069607d8b8f02233
Cooper Mayne <coopermayne@gmail.com>
2013-11-11 21:07:30 -0500

    tweets displaying from ajax

--- app/assets/javascripts/scrapbooks.js
+++ app/assets/javascripts/scrapbooks.js
@@ -5,6 +5,26 @@
 var Scrapbook = {};
 
 
+Scrapbook.queryTwitter = function() {
+  $.ajax({
+    url: "/events/" + Scrapbook.eventId + "/get_tweets",
+    type: "GET",
+    dataType: "json"
+  }).done(function(tweets_array) {
+    $.each(tweets_array, function(index, tweet) {
+      var tweetEl = $('<div></div>').addClass('tweet-container')
+
+      var tweetText = $('<span></span>').addClass('tweet-text')
+      tweetText[0].textContent = tweet.text
+
+      var tweetAuthor = $('<span></span>').addClass('tweet-author')
+      tweetAuthor[0].textContent = tweet.author
+      tweetEl.append(tweetText)
+      tweetEl.append(tweetAuthor)
+      $('#tweets').append(tweetEl)
+    })
+  });
+};
 
 $(document).ready(function() {
   Scrapbook.setEventListeners();
