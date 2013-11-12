ee29a39d57545b46db5f2927ae1b48971515de57
Enoch Riese <enoch.riese@gmail.com>
2013-11-12 13:05:28 -0500

    made basic json crud responses in scrapbooks controller

--- app/controllers/scrapbooks_controller.rb
+++ app/controllers/scrapbooks_controller.rb
@@ -1,10 +1,24 @@
 class ScrapbooksController < ApplicationController
+  respond_to :json, :html
   def show
-
     @scrapbook = Scrapbook.find(params[:id])
     @event = @scrapbook.event
-    #binding.pry
+    respond_with @scrapbook
   end
-
-
+  def create
+    @scrapbook = Scrapbook.create(params[:scrapbook])
+    respond_with @scrapbook
+    redirect_to "/scrapbooks/@scrapbook.id"
+  end
+  def update
+    @scrapbook = Scrapbook.find(params[:id])
+    @scrapbook.update(params[:scrapbook])
+    respond_with @scrapbook
+    redirect_to "/scrapbooks/@scrapbook.id"
+  end
+  def destroy
+    @scrapbook = Scrapbook.find(params[:id])
+    @scrapbook.delete
+    redirect_to "/scrapbooks"
+  end
 end
