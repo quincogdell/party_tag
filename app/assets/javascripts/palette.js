a8a7fb53bb836df0e2332b89219d2da0838ac45f
Enoch Riese <enoch.riese@gmail.com>
2013-11-12 21:59:00 -0500

    committing to look at coop's changes

--- app/assets/javascripts/palette.js
+++ app/assets/javascripts/palette.js
@@ -2,4 +2,6 @@ function changePalette(label) {
   $(".img-container").addClass(label);
   $(".tweet-container").addClass(label);
   $("#canvas").addClass(label);
+  $("img").addClass(label);
+  $(".ui-wrapper").addClass(label);
 }
