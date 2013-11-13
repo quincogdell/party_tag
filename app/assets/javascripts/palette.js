747a015461f95027f6762da8c4d24d3dd289cbea
Enoch Riese <enoch.riese@gmail.com>
2013-11-12 22:48:07 -0500

    changePalette clears dead css, added lightblue

--- app/assets/javascripts/palette.js
+++ app/assets/javascripts/palette.js
@@ -1,7 +1,11 @@
-function changePalette(label) {
-  $(".img-container").addClass(label);
+var palette = {
+  label: null
+};
+
+var changePalette = function(label) {
+  $("." + palette.label).removeClass(palette.label)
+  $(".image-container").addClass(label);
   $(".tweet-container").addClass(label);
   $("#canvas").addClass(label);
-  $("img").addClass(label);
-  $(".ui-wrapper").addClass(label);
+  palette.label = label;
 }
