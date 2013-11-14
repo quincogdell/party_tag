a87212bfeb9b896319bc51e6ab0e3cf91d6292fc
Enoch Riese <enoch.riese@gmail.com>
2013-11-14 16:48:00 -0500

    static buttons up and running

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   setSize();
   $("#canvas").on("mouseup", changeOver);
 });
+
 $(window).resize(function() {setSize();});
 var setSize = function () {
   var clear = $("#nav").height();
@@ -9,19 +10,18 @@ var setSize = function () {
   clear += $("#second").height();
   clear += 20;
   $("#nav-case").css("height", clear);
-  $("#tabs").css("margin-top", clear);
-  clear += $("#tabs").height();
   height = $(window).height() - clear;
-  $("#views").css("height", height);
+  $("#tabs-div").css("height", height);
   var width = $("#container").width();
   width -= $("#canvas").width();
   width -= 12;
-  $("#tabs").css("width", width);
-  $("#tabs-1").width;
-  var size = $("#tabs ul li").width()
+  $("#tabs-div").css("width", width);
+  $("#buttons").css("width", width);
+  var size = $("#buttons ul li").width()
   if (size/7 < 26) {
     $("#tabs ul li").css("font-size", size/7);
   };
+  $("#tab-clear").css("height", $("#buttons").height());
 }
 var changeOver = function() {
   if ($("#container").hasClass("no-books")) {
